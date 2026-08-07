07c32ac2eb9a1fa19410321cf66a0b68dda4d934
Fadli Ashari <ashari.attamimi@gmail.com>
2026-08-07 10:31:55 +0700

    Develop hero section

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -1,17 +1,48 @@
+import { Button } from "../ui/button"
+
 const Hero = () => {
   return (
     <>
       <div className="hero bg-secondary px-5 py-20">
-        <div className="container mx-auto flex">
-          <div className="flex basis-1/2">
-            <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-            <p>
+        <div className="container mx-auto flex items-center justify-start gap-10 px-2">
+          <div className="flex basis-7/12 flex-col justify-start">
+            <h1 className="mb-4 text-5xl">
+              FIND CLOTHES THAT MATCHES YOUR STYLE
+            </h1>
+            <p className="mb-6 text-muted-foreground">
               Browse through our diverse range of meticulously crafted garments,
-              designed to bring out your individually and cater your sense of
+              designed to bring out your individually and cater to your sense of
               style.
             </p>
+
+            <div className="flex">
+              <Button className="mb-10" size="xl">
+                Shop Now
+              </Button>
+            </div>
+
+            <div className="grid grid-cols-3 gap-4">
+              <div className="border-r-2">
+                <p className="text-4xl font-bold text-foreground">200+</p>
+                <p className="text-md text-muted-foreground">
+                  International Brands
+                </p>
+              </div>
+              <div className="">
+                <p className="text-4xl font-bold text-foreground">2.000+</p>
+                <p className="text-md text-muted-foreground">
+                  High-Quality Products
+                </p>
+              </div>
+              <div className="">
+                <p className="text-4xl font-bold text-foreground">30.000+</p>
+                <p className="text-md text-muted-foreground">Happy Customers</p>
+              </div>
+            </div>
           </div>
-          <div className="flex basis-1/2">Content</div>
+          <div className="flex basis-5/12 items-center justify-center">
+            Image
+          </div>
         </div>
       </div>
     </>
