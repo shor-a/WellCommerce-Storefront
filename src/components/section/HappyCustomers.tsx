866ac9aa46be657e6e9ccde56c5bea055eb099fd
Fadli Ashari <ashari.attamimi@gmail.com>
2026-08-11 12:23:02 +0700

    Update padding

--- src/components/section/HappyCustomers.tsx
+++ src/components/section/HappyCustomers.tsx
@@ -23,7 +23,7 @@ const HappyCustomers = (): React.ReactNode => {
   }
 
   return (
-    <section className="happy-customers w-full bg-background pt-20">
+    <section className="happy-customers w-full bg-background pt-30 pb-20">
       <div className="container mx-auto px-10">
         <header className="mb-10 flex w-full max-w-[1280px] items-end justify-between gap-6 px-6">
           <h2 className="mt-[-1px] text-3xl leading-none font-bold tracking-[0] text-black sm:text-5xl">
